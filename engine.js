66324d67d6026e60335f87d93330eb5a57fccca5
Zelenyy <dangerdude119@gmail.com>
2018-07-30 23:13:59 +1000

    Changed shit
    > Added a single "command" to test if it works (it does)

--- engine.js
+++ engine.js
@@ -2,13 +2,20 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const token = require("./token.json");
 const contants = require("./constants.json");
-import "startup.js";
-
+//import "startup.js";
 
 client.on("ready", async=> {
+    console.log(`\n\n\n\n\n BOT STARTED...`);
 
-    console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
     client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
+
+client.on("message", async msg => {
+    if (msg.toString() == "killme") {
+        msg.channel.send("*same tbh*");
+    }
+});
+
+client.login(token.token);
 
 
