41dc4260457826130e3de56bce8abd27c961c6e3
Zelenyy <dangerdude119@gmail.com>
2018-07-31 21:10:55 +1000

    Added logging
    > Added logging and logs all commands typed

--- engine.js
+++ engine.js
@@ -1,20 +1,28 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const token = require("./token.json");
-const contants = require("./constants.json");
+const constants = require("./constants.json");
 const logger = require("./logging")
 
 client.on("ready", async=> {
-    console.log("\n\n\n\n\n BOT STARTING...")
-    client.user.setActivity(`Serving ${client.guilds.size} servers`);
+    console.log("BOT STARTING...")
+    logger.log("LOGGING TEST");
+    logger.log("LOG");
+    logger.warn("WARN");
+    logger.crit("CRIT");
+
+    client.user.setActivity(`killme please`);
 });
 
-client.on("message", async msg => {
-    if (msg.toString() == "killme") {
-        msg.channel.send("*same tbh*");
-    }
+ client.on("message", async msg => {
+    // Return if author is bot or message is not command
+    if (msg.author.bot) return;
+    if (msg.content.indexOf('k')) return;
+
+    const args = msg.content.slice(constants.prefix.length).trim().split(" ");
+    const command = args.shift().toLocaleLowerCase();
+
+    logger.log(msg.author.tag + " used command: [" + command + "] with args: [" + args.toLocaleString() + "]");
 });
 
 client.login(token.token);
-
-
--- logging.js
+++ logging.js
@@ -1,7 +1,27 @@
+var fs = require('fs');
 
 module.exports = {
     log: function(string) {
         //var date = new Date().toTimeString + " - " + new Date().toTimeString;
-        console.log(new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + " [verbose] : " + string);
+        string = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + " [verbose] : " + string;
+        console.log(string);
+        LogFile(string);
     },
+    
+    warn: function(string) {
+        string = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + " [WARNING] : " + string;
+        console.log("\033[33;1m" + string + "\033[0m");
+        LogFile(string);
+    },
+
+    crit: function(string) {
+        string = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + " [CRITICAL] : " + string;
+        console.log("\033[31m" + string + "\033[0m");
+        LogFile(string);
+    }      
 };
+function LogFile(string) {
+    fs.appendFile('logs/' + new Date().toDateString() + '.txt', string + "\n", function (err) {
+        if (err) throw err;
+    });
+}
