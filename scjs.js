c9ae4e643579d935b12cd3dc1c8d1439127bf59b
Zelenyy <dangerdude119@gmail.com>
2018-09-25 17:30:16 +1000

    awooo!

--- scjs.js
+++ scjs.js
@@ -4,6 +4,7 @@ const token = require("./token.json");
 const constants = require("./constants.json");
 const logger = require("./logging");
 const commands = require("./commands");
+const util = require("./utilities.js");
 
 client.on("ready", async=> {
     logger.log("\nBOT START...\n")
@@ -13,6 +14,13 @@ client.on("ready", async=> {
  client.on("message", async msg => {
     // Return if author is bot or message is not command
     if (msg.author.bot) { return; }
+    if(msg.content[0].toLocaleLowerCase() == 'a' && msg.content[1].toLocaleLowerCase() == 'w' && util.CheckOs(msg.content.toLocaleLowerCase(),2)) {
+        msg.react(":awoo:494043211023777793").catch((err) => {
+            logger.warn("server does not have 'awoo emote', " + err);
+        })
+
+        //msg.channel.send("<:awoo:494043211023777793>");
+    }
     if (msg.content.toLowerCase().indexOf(constants.prefix)) { return; }
 
     //format args and command
