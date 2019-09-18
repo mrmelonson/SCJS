a6bdce6db0ab4d14ac78bc1cd979275b9bd52e79
Zelenyy <dangerdude119@gmail.com>
2019-09-18 15:18:37 +1000

    Added new imbeded profile system

--- commands/profile.js
+++ commands/profile.js
@@ -12,9 +12,9 @@ module.exports = {
 	execute(message, args, client) {
         var member = message.mentions.members.first() || message.guild.members.get(args[1]);
         if (member) {
-            member = member.user;
+            member = member;
         } else if (!member) {
-            member = message.author;
+            member = message.member;
         }
 
         MongoClient.connect(url, function(err, db) {
@@ -22,30 +22,34 @@ module.exports = {
             var dbo = db.db("discord_test");
             var query = {id: `${member.id}`};
             
-            dbo.collection(`${message.guild.id}`).findOne(query, function(err, object) {
+            dbo.collection(`${message.guild.id}`).findOne(query, function(err, user) {
                 if (err) throw err;
 
                 var snugreceive = 0;
                 var snuggive = 0;
                 var clowncount = 0;
                 var lonelylvl = 0;
-
-                if (object.snuggive) {
-                    snuggive = object.snuggive;
+                var description = "This user has not set up a profile description yet!";
+                if (user.snuggive) {
+                    snuggive = user.snuggive;
                 }
 
-                if (object.snugreceive) {
-                    snugreceive = object.snugreceive
+                if (user.snugreceive) {
+                    snugreceive = user.snugreceive;
                 }
 
-                if (object.clowncount) {
-                    clowncount = object.clowncount;
+                if (user.clowncount) {
+                    clowncount = user.clowncount;
                 }
 
-                if (object.lonelylvl) {
-                    lonelylvl = object.lonelylvl;
+                if (user.lonelylvl) {
+                    lonelylvl = user.lonelylvl;
                 }
 
+                if(user.profile) {
+                    description = user.desc;
+                }
+                /*
                 message.channel.send("```" +
                                     `Username: ${member.username}\n` +
                                     `\n` +
@@ -54,6 +58,41 @@ module.exports = {
                                     `# of times clowned: ${clowncount}\n` +
                                     `Loneliness Level: ${lonelylvl}` +
                                     "```");
+                */
+                message.channel.send({embed : {
+                    "title": "**Profile**",
+                    "description": description,
+                    "color": member.highestRole.color,
+                    "timestamp": new Date(),
+                    "thumbnail": {
+                        "url": member.user.avatarURL
+                    },
+                    "author": {
+                        "name": member.user.username,
+                        "icon_url": member.user.avatarURL
+                    },
+                    "fields": [
+                        {
+                            "name": "Snugs recieved",
+                            "value": `You have received ${snugreceive} snugs!`,
+                            "inline": true
+                        },
+                        {
+                            "name": "Snugs given",
+                            "value": `You have given ${snuggive} snugs!`,
+                            "inline": true
+                        },
+                        {
+                            "name": "Loneliness level",
+                            "value": `Your loneliness level is: ${lonelylvl}`
+                        },
+                        {
+                            "name": "Clowned",
+                            "value": `You have been clowned on ${clowncount} times!`
+                        },
+                    ]
+                }});
+                
 
             });
             db.close();
