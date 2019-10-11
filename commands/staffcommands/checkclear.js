377a62909067d85e643446e0b51ae2a10d8ffc01
Zelenyy <dangerdude119@gmail.com>
2019-10-11 16:35:15 +1100

    gramma error

--- commands/staffcommands/checkclear.js
+++ commands/staffcommands/checkclear.js
@@ -3,7 +3,7 @@ const consts = require("../../constants.json");
 
 module.exports = {
 	name: 'clearlvl',
-	description: 'Check Your clearance',
+	description: 'Check your clearance level',
 	syntax: `\`${consts.prefix}clearlevel\``,
     clearlvl: 1,
     aliases: ['clearlevel', 'clearance'],
